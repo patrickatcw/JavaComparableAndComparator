a41f467bac18f90d1e8fd0a53fe06ee604411c96
patrickatcw <pj8392@yahoo.com>
2018-05-05 11:42:25 -0400

    base program before we start adding steps to demonstrate comaparable / comparator

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,10 +1,55 @@
 package com.me;
 
+//initial code from collectionslistmethods, with a little clean up
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Theater theater = new Theater("Severance Hall", 8, 12);
+
+        //reserve a seat
+        if (theater.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat is already reserved");
+        }
+
+        if (theater.reserveSeat("D12")) {
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Seat is already reserved");
+        }
+
+        if (theater.reserveSeat("B13")) {
+            System.out.println("Please pay for B13");
+        } else {
+            System.out.println("Seat is already reserved");
+        }
+
     }
 
+    //method that prints out the list
+    public static void printList(List<Theater.Seat> list) {
+        for (Theater.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber() + " $" + seat.getPrice());
+
+        }
+        System.out.println();
+        System.out.println("-----------------------------");
+
+    }
 
 }
+
+/*
+initial code test print before steps for this program
+Seat D12 reserved
+Please pay for D12
+Seat is already reserved
+There is no seat B13
+Seat is already reserved
+ */
+
---
+++ src/com/me/Theater.java
@@ -0,0 +1,95 @@
+package com.me;
+
+import java.util.*;
+
+public class Theater {
+    private final String theaterName;
+    private List<Seat> seats = new ArrayList<>();
+
+
+    public Theater(String theaterName, int numRows, int seatsPerRow) {
+        this.theaterName = theaterName;
+
+        int lastRow = 'A' + (numRows -1);
+        for (char row = 'A'; row <= lastRow; row++) {
+            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+                double price = 12.00;
+
+                if((row < 'D') && (seatNum >=4 && seatNum <=9)) {
+                    price = 14.00;
+                } else if((row > 'F') || (seatNum < 4 || seatNum > 9)) {
+                    price = 7.00;
+                }
+
+                Seat seat = new Seat(row + String.format("%02d", seatNum), price);
+                seats.add(seat);
+            }
+        }
+    }
+
+    public String getTheaterName() {
+        return theaterName;
+    }
+
+    public boolean reserveSeat(String seatNumber) {
+        Seat requestedSeat = new Seat(seatNumber, 0);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+    }
+
+    // for testing
+    public Collection<Seat> getSeats() {
+        return seats;
+    }
+
+    public class Seat implements Comparable<Seat> {
+        private final String seatNumber;
+        private double price;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber, double price) {
+            this.seatNumber = seatNumber;
+            this.price = price;
+        }
+
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
+        }
+
+        public boolean reserve() {
+            if(!this.reserved) {
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        //method cancel
+        public boolean cancel() {
+            if(this.reserved) {
+                this.reserved = false;
+                System.out.println("Reservation of seat " + seatNumber + " cancelled");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+
+}
