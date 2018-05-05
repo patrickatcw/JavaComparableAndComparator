68d4827bdad1de95bbcf878b1fb661fb3cf61cd4
patrickatcw <pj8392@yahoo.com>
2018-05-05 11:48:16 -0400

    step 2 create a comparator instance, override was automatic

--- src/com/me/Theater.java
+++ src/com/me/Theater.java
@@ -6,6 +6,25 @@ public class Theater {
     private final String theaterName;
     private List<Seat> seats = new ArrayList<>();
 
+    //step 2 create a comparator instance, override was automatic
+    static final Comparator<Seat> PRICE_ORDER;
+    //produced as a result of yellow bulb, below...
+    static {
+        PRICE_ORDER = new Comparator<Seat>() {
+            @Override
+            public int compare(Seat seat1, Seat seat2) {
+                if (seat1.getPrice() < seat2.getPrice()) {
+                    return -1;
+                } else if (seat1.getPrice() > seat2.getPrice()) {
+                    return 1;
+                } else {
+                    return 0;
+
+                    //step 3 to main to produce some code to test this
+                }
+            }
+        };
+    }
 
     public Theater(String theaterName, int numRows, int seatsPerRow) {
         this.theaterName = theaterName;
